8ede1c6243e093ef1315ed426d4734743c599128
Mark Ivory <markivory2973@gmail.com>
2026-08-26 09:58:46 +0800

    fix: check if the IP exists

--- pkg/cidrs/cidrs.go
+++ pkg/cidrs/cidrs.go
@@ -2,21 +2,21 @@ package cidrs
 
 import (
 	"context"
-	"net/netip"
 	"ech-injector/pkg/resolvers"
+	"net/netip"
 )
 
 func contains(context context.Context, prefixes []netip.Prefix, name string) (bool, error) {
 	ips, err := resolvers.ResolveA(context, name)
-	if err != nil {
+	if err != nil || len(ips) == 0 {
 		ips, err = resolvers.ResolveAAAA(context, name)
 		if err != nil {
 			return false, err
 		}
-	}
 
-	if len(ips[0]) == 0 {
-		return false, err
+		if len(ips) == 0 {
+			return false, nil
+		}
 	}
 
 	address, err := netip.ParseAddr(ips[0])
