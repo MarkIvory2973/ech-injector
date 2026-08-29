d1f38cd5e7b32dc43a8ef5efe89dcc4b258a0120
Mark Ivory <markivory2973@gmail.com>
2026-08-29 22:53:30 +0800

    fix: stop caching all DNS queries

--- internal/injectors/echconfig.go
+++ internal/injectors/echconfig.go
@@ -3,27 +3,46 @@ package injectors
 import (
 	"context"
 	"ech-injector/pkg/resolvers"
+	"ech-injector/pkg/workers"
+	"encoding/base64"
 	"fmt"
 
 	"github.com/miekg/dns"
 )
 
 func getECHConfig(context context.Context, name string) ([]byte, error) {
-	svcbs, err := resolvers.ResolveHTTPS(context, name)
+	content, err := workers.CacheFunc(name, func() (map[string]string, int, error) {
+		svcbs, err := resolvers.ResolveHTTPS(context, name)
+		if err != nil {
+			return nil, 0, err
+		}
+
+		for _, svcb := range svcbs {
+			for _, value := range svcb {
+				svcbECHConfig, ok := value.(*dns.SVCBECHConfig)
+				if !ok {
+					continue
+				}
+
+				echConfig := base64.StdEncoding.EncodeToString(svcbECHConfig.ECH)
+				content := map[string]string{
+					"echConfig": echConfig,
+				}
+
+				return content, 0, err
+			}
+		}
+
+		return nil, 3600, fmt.Errorf("ECH config not found")
+	})
 	if err != nil {
 		return nil, err
 	}
 
-	for _, svcb := range svcbs {
-		for _, value := range svcb {
-			svcbECHConfig, ok := value.(*dns.SVCBECHConfig)
-			if !ok {
-				continue
-			}
-
-			return svcbECHConfig.ECH, nil
-		}
+	echConfig, err := base64.StdEncoding.DecodeString(content["echConfig"])
+	if err != nil {
+		return nil, err
 	}
 
-	return nil, fmt.Errorf("ECH config not found")
+	return echConfig, nil
 }
--- pkg/resolvers/messages.go
+++ pkg/resolvers/messages.go
@@ -2,10 +2,7 @@ package resolvers
 
 import (
 	"context"
-	"crypto/sha256"
 	"ech-injector/pkg/workers"
-	"encoding/base64"
-	"encoding/hex"
 
 	"github.com/miekg/dns"
 )
@@ -30,58 +27,24 @@ func UnpackMessage(content []byte) (*dns.Msg, error) {
 }
 
 func ExchangeMessage(context context.Context, dnsQuestion *dns.Msg) (*dns.Msg, error) {
-	id := dnsQuestion.Id
-	dnsQuestion.Id = 0
-
 	content, err := PackMessage(dnsQuestion)
 	if err != nil {
 		return nil, err
 	}
 
-	checksum := sha256.Sum256(content)
-	key := hex.EncodeToString(checksum[:])
-	cache, err := workers.CacheFunc(key, func() (map[string]string, int, error) {
-		httpRequest := workers.HTTPRequest{
-			Method:  "POST",
-			Scheme:  "https",
-			Host:    host,
-			Path:    path,
-			Queries: map[string]string{},
-			Headers: map[string]string{
-				"Content-Type": "application/dns-message",
-				"Accept":       "application/dns-message",
-			},
-			Content: content,
-		}
-		content, err = workers.Fetch(context, httpRequest)
-		if err != nil {
-			return nil, 0, err
-		}
-
-		cache := map[string]string{
-			"answer": base64.StdEncoding.EncodeToString(content),
-		}
-
-		dnsAnwser, err := UnpackMessage(content)
-		if err != nil {
-			return nil, 0, err
-		}
-
-		var minTTL int
-		for _, resourceRecord := range dnsAnwser.Answer {
-			ttl := int(resourceRecord.Header().Ttl)
-			if minTTL == 0 || ttl < minTTL {
-				minTTL = ttl
-			}
-		}
-
-		return cache, minTTL, nil
-	})
-	if err != nil {
-		return nil, err
+	httpRequest := workers.HTTPRequest{
+		Method:  "POST",
+		Scheme:  "https",
+		Host:    host,
+		Path:    path,
+		Queries: map[string]string{},
+		Headers: map[string]string{
+			"Content-Type": "application/dns-message",
+			"Accept":       "application/dns-message",
+		},
+		Content: content,
 	}
-
-	content, err = base64.StdEncoding.DecodeString(cache["answer"])
+	content, err = workers.Fetch(context, httpRequest)
 	if err != nil {
 		return nil, err
 	}
@@ -90,8 +53,6 @@ func ExchangeMessage(context context.Context, dnsQuestion *dns.Msg) (*dns.Msg, e
 	if err != nil {
 		return nil, err
 	}
-
-	dnsAnswer.Id = id
 
 	return dnsAnswer, nil
 }
